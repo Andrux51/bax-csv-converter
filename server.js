e2f0e36bc194f83079becc08b429743ba28eb2ae
Andy Martin <anmartin@encore.corp.root>
2017-06-15 14:57:53 -0600

    set up node api for txt input

---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const hapi = require('hapi');
+const fs = require('fs');
+
+const server = new hapi.Server();
+server.connection({ port: 5000, host: 'localhost' });
+
+server.register(require('inert'), (err) => {
+    if (err) throw err;
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (req, res) => {
+            res.file('./index.html');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/public/{path*}',
+        handler: {
+            directory: {
+                path: './',
+                listing: false,
+                index: false
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/txtfile',
+        handler: (req, res) => {
+            res.file('bax-output.txt');
+        }
+    });
+
+    // take in uploaded txt file to hold
+    server.route({
+        method: 'POST',
+        path: '/import',
+        handler: (req, res) => {
+            var createdFileName = 'batch_' + Date.now() + '.txt';
+
+            fs.stat('uploads', (err, stats) => {
+                if (err) fs.mkdirSync('uploads');
+
+                fs.writeFile('./uploads/' + createdFileName, req.payload, 'utf8', () => {
+                    res('Saved to file ' + createdFileName).code(201);
+                });
+            });
+        }
+    });
+
+    // export generated CSV content into file
+    server.route({
+        method: 'POST',
+        path: '/export',
+        handler: (req, res) => {
+            fs.stat('output', (err, stats) => {
+                if(err) fs.mkdirSync('output');
+
+                // do stuff with req.payload
+                res('Saved').code(201);
+            });
+        }
+    })
+});
+
+server.start((err) => {
+    if (err) throw err;
+
+    console.log(`Server running at: ${server.info.uri}`);
+});
